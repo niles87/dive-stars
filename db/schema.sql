f3d23904ca606ccfd9a2bb6dcd60c1738d96df32
Niles Bingham <nbing19@gmail.com>
2022-06-15 10:15:37 -0700

    fixes #2, added new tables to schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,6 @@ drop DATABASE if EXISTS diving_db;
 
 create DATABASE diving_db;
 
-use DATABASE diving_db;
-
 create domain UNSIGNED as Integer check (value > 0);
 
 create Table certifications (
@@ -22,3 +20,44 @@ CREATE TABLE divers (
     SET
         NULL
 );
+
+create domain LATLONG as POINT check (
+    value [0] between -90
+    and 90
+) check (
+    value [1] between -180
+    and 180
+);
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    coordinates LATLONG NOT NULL
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    location_id INTEGER NOT NULL REFERENCES locations(id) ON DELETE CASCADE,
+    UNIQUE(name, location_id)
+);
+
+CREATE TABLE dives (
+    id SERIAL PRIMARY KEY,
+    depth NUMERIC(5, 2) NOT NULL,
+    dive_date TIMESTAMP NOT NULL DEFAULT NOW(),
+    duration UNSIGNED NOT NULL,
+    diver_id INTEGER NOT NULL REFERENCES divers(id) ON DELETE CASCADE,
+    location_id INTEGER NOT NULL REFERENCES locations(id) ON DELETE CASCADE
+);
+
+create Function random_between(low int, high int) returns 
+int as 
+$$ 
+BEGIN 
+	RETURN floor(random() * (high - low + 1) + low);
+	end;
+	$$ language 
+plpgsql 
+
+;
